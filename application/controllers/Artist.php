4e2fe7f81f6926ee52563316162a4489b721c8ef
Sam <sam.fullen@intouchnetworks.com>
2017-03-17 08:42:51 +0000

    General skeleton

---
+++ application/controllers/Artist.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+ini_set('max_execution_time', 0); 
+ini_set('memory_limit','2048M');
+
+class Artist extends CI_Controller {
+
+	public function index()
+	{
+		
+	}
+
+}
---
+++ application/controllers/Note.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+ini_set('max_execution_time', 0); 
+ini_set('memory_limit','2048M');
+
+class Note extends CI_Controller {
+
+	public function index()
+	{
+		
+	}
+
+}
